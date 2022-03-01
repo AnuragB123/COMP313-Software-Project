e0d1a3f603be75d7ad5e37136082828fde2df299
vaishali-vcs <vaishali.bhat@yahoo.com>
2022-03-01 16:53:55 -0500

    added user creation logic
    added user creation logic

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -38,3 +38,29 @@ module.exports.findUser= (req, res, next) => {
         res.status(200).json({ message: "User Update successful!"});
       });
   }
+
+  // function to insert User into DB
+module.exports.addUser= (req, res, next) => {
+  
+  let newuser = Users({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    userType: req.body.userType,
+    phone: req.body.phone,
+    isTutor: req.body.isTutor
+  });
+
+  Users.create(newuser, (err, newuser) =>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else
+    {
+      res.status(200).json({success: true, msg: 'Successfully added newuser'});
+    }
+});
+
+}
