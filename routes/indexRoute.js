4227ceaf66e1c7d78bc8b18a90c616a35622771b
AnuragB123 <abhatt66@my.centennialcollege.ca>
2022-03-05 12:32:46 -0500

    Updated App File, Added neccessary router files and commented steps to do for Login View

---
+++ routes/indexRoute.js
@@ -0,0 +1,18 @@
+//Load User Controller
+var users = require('../controllers/UserController');
+var express = require('express');
+var router = express.Router();
+
+module.exports = function (app) {
+    //Users parameterized routes
+    app.route('/users/:userId')
+    .get(users.findUser)
+
+    app.params('userId', users.userById) //Show Specific User Controller Function must be created
+
+    app.post('/index', users.authenticate) //Authentication of User Function in Controller must be created
+    app.get('/signOut',users.signout) //Signing out Functiin in Controller must be created
+
+    app.get('/profile',users.profile) //Redirect to Profile Page Function in Controller must be created
+
+}
