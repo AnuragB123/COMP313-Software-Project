b121258276a9102252ba36228ec0e855dc4a8167
JackSparrow114 <arpitshukla114@yahoo.co.in>
2022-04-14 19:28:48 -0400

    added addgrade logic

--- routes/graderRoute.js
+++ routes/graderRoute.js
@@ -5,4 +5,5 @@ var express = require('express');
 var router = express.Router();
 //Routes to all pages involving User
 router.get('/getGrades', graderController.getGraderPage);
+router.post('/addGrade', graderController.addGrade);
 module.exports = router;
