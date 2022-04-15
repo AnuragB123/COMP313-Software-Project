d3edf3298c1a820b4a1631cfc995f682e8064c64
AnuragB123 <52175974+AnuragB123@users.noreply.github.com>
2022-04-15 14:19:25 -0400

    Teacher Grader Page Completed

--- public/JavaScript/teacherGrader.js
+++ public/JavaScript/teacherGrader.js
@@ -5,3 +5,42 @@ function saveGrade() {
     document.getElementById('studentId').value = student.value
     document.getElementById('studentName').value = student.text;
 }
+
+function setGrade() {
+    var mark = document.getElementById('studentMark');
+    var grade = document.getElementById('studentGrade');
+    var markValue = mark.value;
+    if (markValue >= 0 && markValue <= 49){
+        grade.value = "F";
+    }
+    else if (markValue >= 50 && markValue <=54){
+        grade.value = "D";
+    }
+    else if (markValue >= 55 && markValue <= 59){
+        grade.value = "D+";
+    }
+    else if (markValue >= 60 && markValue <= 64){
+        grade.value = "C";
+    }
+    else if (markValue >= 65 && markValue <= 69){
+        grade.value = "C+";
+    }
+    else if (markValue >= 70 && markValue <= 74){
+        grade.value = "B";
+    }
+    else if (markValue >= 75 && markValue <= 79){
+        grade.value = "B+";
+    }
+    else if (markValue >= 80 && markValue <= 89){
+        grade.value = "A";
+    }
+    else if (markValue >= 90 && markValue <= 100){
+        grade.value = "A+"
+    }
+    else
+    {
+        grade.value = null;
+        alert("Invalid mark");
+        mark.value = null;
+    }
+}
